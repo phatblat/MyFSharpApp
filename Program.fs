e3a97eb9d7a3f8b6b409c1fa8d600caf44e5f084
Ben Chatelain <ben@octop.ad>
2022-03-09 14:50:31 -0700

    ✨ Add function

--- Program.fs
+++ Program.fs
@@ -1,2 +1,8 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+// Function to print a name
+let printGreeting name =
+    printfn $"Hello {name} from F#!"
+
+// Call function
+printGreeting "Ana"
